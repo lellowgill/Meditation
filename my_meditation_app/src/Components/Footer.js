4bf8a0668779ce79254c54d68d2084611e72a747
deb <lellowgill@gmail.com>
2021-11-29 14:34:47 -0500

    added footer to javascript

---
+++ my_meditation_app/src/Components/Footer.js
@@ -0,0 +1,11 @@
+import react from 'react';
+import '../App.css';
+
+export default function Footer() {
+    return (
+        <div className="footer">
+        <p>Copyright © 2021 Stoneccd, LLC. All Rights Reserved</p>
+    </div>
+    )
+}
+
