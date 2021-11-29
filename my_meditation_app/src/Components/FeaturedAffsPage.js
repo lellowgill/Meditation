d53b18532151b3abea01e6fc69a9fc5ffbcdf69b
deb <lellowgill@gmail.com>
2021-11-29 14:39:34 -0500

    added a new component need style added

---
+++ my_meditation_app/src/Components/FeaturedAffsPage.js
@@ -0,0 +1,11 @@
+import react from 'react';
+import '../App.css';
+
+const FeaturedAffsPage = () => {
+    return (
+        <div className="list-container">
+
+        </div>
+    )
+}
+export default FeaturedAffsPage;
