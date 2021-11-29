ed090e8f81900afffd485f086d68ddfa97777a55
deb <lellowgill@gmail.com>
2021-11-29 12:23:41 -0500

    added the div for motto

---
+++ my_meditation_app/src/Components/Motto.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import 'App.css';
+
+export const Motto = () => {
+    return (
+        <section>
+    <div>
+        <h2>"I am because I choose to be"</h2>
+    </div>
+</section>
+    )
+}
