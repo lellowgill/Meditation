aad88cbe33e03bf06b75e0e00ebba5d9312b143d
deb <lellowgill@gmail.com>
2021-11-29 14:33:14 -0500

    import js

---
+++ my_meditation_app/src/Components/Header.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import '../App.css';
+
+export default function Header(){
+    return (
+        <div className="Header">
+                <div>The</div> 
+                <div>Affirmeditation</div>
+                <div>Shop</div>
+        </div> 
+    )
+}
